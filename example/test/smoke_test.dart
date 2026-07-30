30a87d5e9e4c0d6ff0c302ce9154693239b59829
chahyasantoso <chahya.santoso@gmail.com>
2026-07-30 21:04:00 +0700

    test(example): add a package smoke test

---
+++ example/test/smoke_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:motionpath_flutter_example/main.dart';
+
+void main() {
+  test('example entrypoint is importable', () {
+    expect(const MotionPathExampleApp(), isA<MotionPathExampleApp>());
+  });
+}
