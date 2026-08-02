dcb36dd4e5912b3a9638c02b62d6cdd6ddaa8060
chahyasantoso <chahya.santoso@gmail.com>
2026-08-02 12:36:27 +0700

    test(core): keep the yoyo midpoint expectation correct

--- packages/motionpath_core/test/trigger_publisher_test.dart
+++ packages/motionpath_core/test/trigger_publisher_test.dart
@@ -10,7 +10,7 @@ void main() {
     );
     expect(trigger.progressAt(0.5, 1), 0);
     expect(trigger.progressAt(1.5, 1), 0.5);
-    expect(trigger.progressAt(2.5, 1), 0);
+    expect(trigger.progressAt(2.5, 1), 0.5);
   });
 
   test('holds the playhead at the end during repeat delay', () {
